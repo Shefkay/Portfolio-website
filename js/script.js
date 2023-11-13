c004cfe9704cedc1ac47094c2e363147e3706720
shefkay <sherifayantoyinbo@gmail.com>
2023-11-13 12:28:35 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ========================== typing animation ========================== */
+var typed = new Typed(".typing", {
+  strings: ["", "FrontEnd Developer", "Data Analysis", "Graphic Designer", "YouTuber"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+})
